ff24cb842b0adb7bfe0a5a30a19baa4e9b5b8797
Navinda Chandrasiri <adilanga@gmail.com>
2023-06-21 15:53:29 +0530

    Registration Controller Impl

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,7 +1,36 @@
 class RegistrationController < ApplicationController
+  # User Registration Page Load
   def index
+    @user = User.new
   end
 
+  # User Registration Method
   def create
+    # Initialized user object with params data
+    @user = User.new(user_params)
+
+    begin
+      # Saving user in DB
+      if @user.save
+        # Save user id in session storage if user saved in database
+        session[:user_id] = @user.id
+
+        # Redirect to Root Path and Notify user registration
+        redirect_to root_path, notice: 'User Successfully Registered!'
+      else
+        # If User didn't registered load same page and notify errors
+        render :index, status: :unprocessable_entity
+      end
+    rescue => e
+      # Logging errors when user registration
+      logger.error("User Registration Error: #{e.message}")
+    end
+  end
+
+  private
+
+  # User Params for new user create
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 end
