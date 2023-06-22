6b33a3c42f021588f400a83c3bd55a33b21b59cc
Navinda Chandrasiri <adilanga@gmail.com>
2023-06-22 10:51:46 +0530

    Registration Test

--- test/controllers/registration_controller_test.rb
+++ test/controllers/registration_controller_test.rb
@@ -1,13 +1,23 @@
 require "test_helper"
 
 class RegistrationControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get registration_index_url
+
+  # Test whether registration view load
+  test "should view registration page" do
+    get registration_url
+
+    # Assert Check response is success
     assert_response :success
   end
 
-  test "should get create" do
-    get registration_create_url
-    assert_response :success
+  # Test whether registration with sample data
+  test "should register user" do
+    # Check User count on creation
+    assert_difference("User.count") do
+      post registration_url, params: { user: { name: 'User One', username: 'sample11', email: 'user11@email.com', password: 'Password@123', password_confirmation: 'Password@123' } }
+    end
+
+    # Check redirection when user registration done
+    assert_redirected_to root_url
   end
 end
